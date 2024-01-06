82a117d426a83638dc725b3b0128c251c3c4b810
Khanh204 <110509760+Khanh204@users.noreply.github.com>
2024-01-06 13:58:33 +0700

    invoice

--- src/App.js
+++ src/App.js
@@ -19,6 +19,8 @@ import Search from './components/pages/search';
 import Searching from './components/pages/searching';
 import Cart from './components/pages/cart';
 import Menu from './components/pages/menu';
+import Invoice from './components/pages/invoice';
+
 
 function App() {
     const location = useLocation();
@@ -42,6 +44,7 @@ function App() {
         <Route path='/register' element={<Register/>}/>
         <Route path='/reset-password' element={<ResetPassword/>}/>
         <Route path='/search' element={<Search/>}/>
+        <Route path='/invoice' element={<Invoice/>}/>
       </Routes>
       <Footer currentLocation={location.pathname}></Footer>
       </div>
