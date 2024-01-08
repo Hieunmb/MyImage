de93a4e5f6838ad6cdbdec24ad9d4b436b79c024
Nguyễn Hiếu <zhuongdae@gmail.com>
2024-01-08 13:02:02 +0700

    url

--- src/services/url.js
+++ src/services/url.js
@@ -10,6 +10,20 @@ const url ={
         POST:"/Image",
         PUT:"/Image",
         DELETE:"/Image",
+    },
+    FRAME:{
+        LIST:"/Frame",
+        CREATE:"/Frame",
+        EDIT:"/Frame",
+        DELETE:"/Frame",
+        GET:"/Frame/get-by-id"
+    },
+    SIZE:{
+        LIST:"/Size",
+        CREATE:"/Size",
+        EDIT:"/Size",
+        DELETE:"/Size",
+        GET:"/Size/get-by-id"
     }
 }
 export default url;
